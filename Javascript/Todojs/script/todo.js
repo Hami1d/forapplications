0fe6e07f64f15326667e834ea48ee9ce03a13363
Hami1d <25hamid04@gmail.com>
2024-02-10 23:34:44 +0100

    comments added

--- Javascript/Todojs/script/todo.js
+++ Javascript/Todojs/script/todo.js
@@ -1,32 +1,35 @@
 //inputs    id= Todoinput   id= Todoaddbutton
 //task  id= Todocontent
 
+// get the input and button
+const Todoinput = document.getElementById("Todoinput");
+const addTodo = document.getElementById("Todoaddbutton");
+const Todocontent = document.getElementById("Todocontent");
 
-const Todoinput = document.getElementById('Todoinput');
-const addTodo = document.getElementById('Todoaddbutton');
-const Todocontent = document.getElementById('Todocontent');
+addTodo.addEventListener("click", () => {
+  // add event listener to the button
+  const task = document.createElement("p"); // create a p tag and a button
+  const btn = document.createElement("button");
+  task.innerText = Todoinput.value; // add the input value to the p tag
+  Todocontent.appendChild(task); // append the p tag to the task div
+  Todoinput.value = ""; // clear the input value
+  btn.innerHTML = "Delete"; // add the delete button
+  Todocontent.appendChild(btn); // append the delete button to the task div
+  btn.style.backgroundColor = "red";
+  btn.style.color = "rgb(255, 255, 255)";
+  btn.style.cursor = "pointer";
 
-
-addTodo.addEventListener('click', () => {
-    const task = document.createElement('p');
-    const btn = document.createElement("button");
-    task.innerText = Todoinput.value
-    Todocontent.appendChild(task);
-    Todoinput.value = "";
-    btn.innerHTML = "Delete";
-    Todocontent.appendChild(btn);
-    btn.style.backgroundColor = 'red';
-    btn.style.color = 'rgb(255, 255, 255)';
-    btn.style.cursor= 'pointer';
-
-    btn.addEventListener('click', () => {
-        Todocontent.removeChild(task)
-        Todocontent.removeChild(btn)
-    });
-    task.addEventListener('click', () => {
-        task.style.textDecoration = "line-through"
-    });
-    task.addEventListener('dblclick', () => {
-        task.style.textDecoration = "none"
-    });
+  btn.addEventListener("click", () => {
+    // add event listener to the delete button
+    Todocontent.removeChild(task); // remove the p tag
+    Todocontent.removeChild(btn); // remove the delete button
+  });
+  task.addEventListener("click", () => {
+    // add event listener to the p tag
+    task.style.textDecoration = "line-through"; // add line-through to the p tag
+  });
+  task.addEventListener("dblclick", () => {
+    // add event listener to the p tag
+    task.style.textDecoration = "none"; // remove line-through from the p tag
+  });
 });
